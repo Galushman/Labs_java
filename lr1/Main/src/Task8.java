8d8109eb95002bcf2611c8516b7100dc99115570
Galushman <galushkomask@gmail.com>
2025-05-16 21:29:34 +0500

    Initial commit

---
+++ lr1/Main/src/Task8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the first number: ");
+        int number1 = in.nextInt();
+
+        System.out.println("Input the second number: ");
+        int number2 = in.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println(number1 + " + " + number2 + " = " + sum);
+    }
+}
