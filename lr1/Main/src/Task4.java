0d39e14b394db64ff377d4bd4f96fb825e68368e
Galushman <galushkomask@gmail.com>
2025-05-16 20:55:06 +0500

    Initial commit

---
+++ lr1/Main/src/Task4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the name of the month: ");
+        String month = in.nextLine();
+
+        System.out.println("Input count of the days in month:");
+        int count = in.nextInt();
+
+        System.out.println(month + " has " + count + " days");
+    }
+}
