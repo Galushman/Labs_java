b5e7f483f2541d0e56029082ec68fc0cca21c495
Galushman <galushkomask@gmail.com>
2025-05-16 21:15:38 +0500

    Initial commit

---
+++ lr1/Main/src/Task6.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Input your year of the birth: ");
+        int year = in.nextInt();
+
+        int age = currentYear - year;
+
+        System.out.println("Your name is " + name + "\nYour age is " + age);
+    }
+}
