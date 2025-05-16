d8a85a8ea076caa4622c4777119cd4df4180a930
Galushman <galushkomask@gmail.com>
2025-05-16 20:47:18 +0500

    Initial commit

---
+++ lr1/Main/src/Task3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the current day of the week: ");
+        String day = in.nextLine();
+
+        System.out.println("Input the name of the current month: ");
+        String month = in.nextLine();
+
+        System.out.println("Input the current date:");
+        int date = in.nextInt();
+
+        System.out.println("Today is " + day + " " + date + " " + month);
+    }
+}
