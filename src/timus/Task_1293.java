ffdefd7fedda9b352acf4063af332a3dac55f666
Galushman <galushkomask@gmail.com>
2025-05-16 22:30:30 +0500

    Initial commit

---
+++ src/timus/Task_1293.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task_1293 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int result = N * A * B * 2;
+
+        System.out.println(result);
+    }
+}
