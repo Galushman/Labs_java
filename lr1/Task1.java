2a4541e7ebeae972175ab19464a4514156d42402
Galushman <galushkomask@gmail.com>
2025-05-16 19:57:40 +0500

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	lr1/Example1.java

---
+++ lr1/Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input second name: ");
+        String secondName = in.nextLine();
+
+        System.out.println("Input first name: ");
+        String firstName = in.nextLine();
+
+        System.out.println("Input patronymic: ");
+        String patronymic = in.nextLine();
+
+        System.out.println("Hello, " + secondName + " " + firstName + " " + patronymic);
+        in.close();
+}
