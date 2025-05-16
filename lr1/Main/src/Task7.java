6ebf190217494eefed5f975a2e1dfd7bc36fb1ff
Galushman <galushkomask@gmail.com>
2025-05-16 21:24:26 +0500

    Initial commit

---
+++ lr1/Main/src/Task7.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input your age: ");
+        int age = in.nextInt();
+
+        int yearOfTheBirth = currentYear - age;
+
+        System.out.println("You were born in " + yearOfTheBirth);
+    }
+}
